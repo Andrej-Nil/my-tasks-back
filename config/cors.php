e21cadcb1c61ef5c9a63343dbc87322995430f4a
AndrejNil <and.kucheroff@yandex.ru>
2026-08-27 17:09:23 +0500

    настроил апи запрос на регистрацию

--- config/cors.php
+++ config/cors.php
@@ -17,13 +17,16 @@ return [
 
     'paths' => [
         'api/*',
+        'register',
+        'login',
+        'logout',
         'sanctum/csrf-cookie',
     ],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
+        'http://app.my-tasks.local:5173',
     ],
 
     'allowed_origins_patterns' => [],
