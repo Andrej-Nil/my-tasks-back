f43005edf5cd1a42877bd3c99fa553c1dd24de51
AndrejNil <and.kucheroff@yandex.ru>
2026-09-16 18:59:58 +0500

    Начинаю CRUD для задач

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,18 +8,9 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function show(Request $request){
-        $user = auth('sanctum')->user();
-
-        if($user){
-            return response()->json([
-                'authenticated' => true,
-                'user' => $user
-            ], 200);
-        }
-
         return response()->json([
-            'authenticated' => false,
-            'user' => null
-        ], 200);
+            'authenticated' => true,
+            'user' => $request->user()
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::get('/user', [UserController::class, 'show']);
+//    Route::get('/tasks', [TaskController::class, 'index']);
 
-Route::get('/user', [App\Http\Controllers\Api\UserController::class, 'show']);
+});
 
-//Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-//
-//// 1. ПУБЛИЧНЫЙ РОУТ (доступен без авторизации)
-//// Ссылка: http://backend.local
-//Route::get('/public-news', function () {
-//    return response()->json([
-//        ['id' => 1, 'title' => 'Проект успешно запущен на Open Server 6!'],
-//        ['id' => 2, 'title' => 'Laravel и React теперь работают в связке.'],
-//    ]);
-//});
-//
-//// 2. ЗАЩИЩЕННЫЙ РОУТ (доступен ТОЛЬКО после успешного входа)
-//// Ссылка: http://backend.local
-//Route::middleware('auth:sanctum')->get('/my-private-tasks', function (Request $request) {
-//    return response()->json([
-//        ['id' => 1, 'task' => 'Секретная задача: Сделать крутое приложение', 'status' => 'В процессе'],
-//        ['id' => 2, 'task' => 'Купить кофе для ночных посиделок за кодом', 'status' => 'Сделано'],
-//        // Laravel Breeze также автоматически знает, КТО залогинился:
-//        ['id' => 3, 'task' => 'Задание лично для пользователя: ' . $request->user()->name, 'status' => 'Важно']
-//    ]);
-//});
